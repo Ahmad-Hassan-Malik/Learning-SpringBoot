ae750c6fac4776b5d0be4311883617c9798f8756
Ahmad-Hassan-Malik <ahmadhassan4714118@gmail.com>
2026-02-06 17:22:38 +0500

    scheduling and cron

--- src/main/java/com/ahmadmalik/mySpringBootProject/MySpringBootProjectApplication.java
+++ src/main/java/com/ahmadmalik/mySpringBootProject/MySpringBootProjectApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableScheduling
 public class MySpringBootProjectApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ahmadmalik/mySpringBootProject/scheduler/UserScheduler.java
@@ -0,0 +1,22 @@
+package com.ahmadmalik.mySpringBootProject.scheduler;
+
+import com.ahmadmalik.mySpringBootProject.service.EmailService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class UserScheduler {
+
+    @Autowired
+    private EmailService emailService;
+
+
+    @Scheduled(cron = "*/5 * * * * *")
+    public void sendEmailToHaji() {
+        emailService.sendEmail("sp24-bse-029@cuilahore.edu.pk","FBI INVESTIGATION", "hi moli ammar kaise ho");
+        log.info("message sent");
+    }
+}
